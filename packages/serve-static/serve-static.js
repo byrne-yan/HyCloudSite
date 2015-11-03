5ba7a39ef775b66aeef283c92fa4234ff5f7f43d
颜俊标 <byrne.yan@yahoo.com>
2015-11-03 17:31:26 +0800

    restore req.url from connectHandlers to req of connect

--- packages/serve-static/serve-static.js
+++ packages/serve-static/serve-static.js
@@ -1,13 +1,11 @@
 // Write your package code here!
 var contentDisposition = Npm.require('content-disposition');
 var finalhandler = Npm.require('finalhandler');
-//var http = Npm.require('http');
 var serveStatic = Npm.require('serve-static');
 
 // Serve up public/ftp folder
 var downloadsFolder = Meteor.settings.DownloadsRoot || process.env['DownloadsRoot'];
-console.log(downloadsFolder);
-debugger;
+
 var serve = serveStatic(downloadsFolder, {
     'index': false,
     'setHeaders': setHeaders
@@ -15,13 +13,12 @@ var serve = serveStatic(downloadsFolder, {
 
 // Set header to force download
 function setHeaders(res, path) {
-    console.log(path);
     res.setHeader('Content-Disposition', contentDisposition(path))
 }
 
-WebApp.connectHandlers.use('/releases',function(req, res, next){
-    console.log(req.originalUrl);
+WebApp.rawConnectHandlers.use('/releases',function(req, res, next){
+    //补回前缀
+    req.url = '/releases' + req.url;
     var done = finalhandler(req, res);
-    //debugger;
     serve(req, res, done)
 });
